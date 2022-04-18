44827b0312f5ab7647e25e893a4831ba97698fdb
Jans Glabs <x00152677@mytudublin.ie>
2022-04-18 12:03:40 +0100

    console class java file

---
+++ app/src/main/java/com/example/androidca2frontend2/Console.java
@@ -0,0 +1,18 @@
+package com.example.androidca2frontend2;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Console {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
